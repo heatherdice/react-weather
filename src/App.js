957c2bbe5b88286c4b911ec76a264d9141f09b8b
Heather Dice <heatherdice22@gmail.com>
2023-05-08 16:39:34 -0400

    Added Bootstrap, moved html to app.js

--- src/App.js
+++ src/App.js
@@ -3,55 +3,63 @@ import React, {useState} from "react";
 import axios from "axios";
 
 export default function App() {
-  let [weather, setWeather] = useState(null);
-  let [city, setCity] = useState(" ");
-
-  function submitInput(event) {
-    event.preventDefault();
-    setCity(event.target.value);
-  }
-
-  function showWeather(response) {
-    setWeather({
-      temp: response.data.temperature.current,
-      description: response.data.condition.description,
-      humidity: response.data.temperature.humidity,
-      wind: response.data.wind.speed,
-      icon: response.data.condition.icon_url
-    });
-  }
-
-  function handleCity(event) {
-    event.preventDefault();
-    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=335d26daoc39f096bf1t1b45c4c341e4&units=imperial`;
-    axios.get(url).then(showWeather);
-  }
-
-  if (weather) {
-    return (
-      <div className="App">
-        <form onSubmit={handleCity}>
-          <input type="text" placeholder="Enter a city..." onChange={submitInput}/>
-          <input type="submit" value="Search"/>
-        </form>
-        <ul>
-          <li>{Math.round(weather.temp)}</li>
-          <li>{weather.description}</li>
-          <li>{weather.humidity}%</li>
-          <li>{Math.round(weather.wind)} mph</li>
-          <li><img src={weather.icon} alt="weather icon"/></li>
-        </ul>
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <p><a href="https://github.com/heatherdice/react-weather">Code</a> by Heather Dice</p>
       </div>
-    );
-  }
-  else {
-    return (
-      <div className="App">
-        <form onSubmit={handleCity}>
-          <input type="text" placeholder="Enter a city..." onChange={submitInput}/>
-          <input type="submit" value="Search"/>
-        </form>
-      </div>
-    );
-  }
+    </div>
+  );
+  // let [weather, setWeather] = useState(null);
+  // let [city, setCity] = useState(" ");
+
+  // function submitInput(event) {
+  //   event.preventDefault();
+  //   setCity(event.target.value);
+  // }
+
+  // function showWeather(response) {
+  //   setWeather({
+  //     temp: response.data.temperature.current,
+  //     description: response.data.condition.description,
+  //     humidity: response.data.temperature.humidity,
+  //     wind: response.data.wind.speed,
+  //     icon: response.data.condition.icon_url
+  //   });
+  // }
+
+  // function handleCity(event) {
+  //   event.preventDefault();
+  //   let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=335d26daoc39f096bf1t1b45c4c341e4&units=imperial`;
+  //   axios.get(url).then(showWeather);
+  // }
+
+  // if (weather) {
+  //   return (
+  //     <div className="App">
+  //       <form onSubmit={handleCity}>
+  //         <input type="text" placeholder="Enter a city..." onChange={submitInput}/>
+  //         <input type="submit" value="Search"/>
+  //       </form>
+  //       <ul>
+  //         <li>{Math.round(weather.temp)}</li>
+  //         <li>{weather.description}</li>
+  //         <li>{weather.humidity}%</li>
+  //         <li>{Math.round(weather.wind)} mph</li>
+  //         <li><img src={weather.icon} alt="weather icon"/></li>
+  //       </ul>
+  //     </div>
+  //   );
+  // }
+  // else {
+  //   return (
+  //     <div className="App">
+  //       <form onSubmit={handleCity}>
+  //         <input type="text" placeholder="Enter a city..." onChange={submitInput}/>
+  //         <input type="submit" value="Search"/>
+  //       </form>
+  //     </div>
+  //   );
+  // }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Weather App</h1>
     <App />
-    <p><a href="https://github.com/heatherdice/react-weather">Code</a> by Heather Dice</p>
   </React.StrictMode>
 );
 
