54628ac1de93f9dd51cb16479e67da9db16d451e
Heather Dice <heatherdice22@gmail.com>
2023-05-10 10:05:32 -0400

    Added search functionality

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
+    const [city, setCity] = useState(props.defaultCity);
     const [weatherData, setWeatherData] = useState({ready: false});
     function handleResponse(response) {
         console.log(response.data);
@@ -19,13 +20,28 @@ export default function Weather(props) {
         });
     }
 
+    function search() {
+        const apiKey = "335d26daoc39f096bf1t1b45c4c341e4";
+        let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`; // city = (use)State
+        axios.get(apiURL).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
+    }
+
+    function handleCityChange(event) {
+        setCity(event.target.value);
+    }
+
     if (weatherData.ready) {
         return (
             <div className="Weather">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row">
                         <div className="col-9">
-                            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/>
+                            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" onChange={handleCityChange}/>
                         </div>
                         <div className="col-3">
                             <input type="submit" value="search" className="btn btn-primary w-100"/>
@@ -37,9 +53,7 @@ export default function Weather(props) {
         );
     }
     else {
-        const apiKey = "335d26daoc39f096bf1t1b45c4c341e4";
-        let apiURL = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
-        axios.get(apiURL).then(handleResponse);
+        search();
         return "Loading...";
     }
 }
