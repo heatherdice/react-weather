645c330e149c0e5a0e9b7baf15d22faae70674f3
Heather Dice <heatherdice22@gmail.com>
2023-05-15 17:49:50 -0400

    Added useEffect to show forecast for searched city

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './Forecast.css';
 import ForecastDay from './ForecastDay';
 import axios from "axios";
@@ -7,10 +7,15 @@ import axios from "axios";
 export default function Forecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    // set loaded to false when the coordinates change
+    // shows forecast for city when new city is searched
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
     
     function handleResponse(response) {
         setForecast(response.data.daily);
-        console.log(response.data.daily);
         setLoaded(true);
     }
 
