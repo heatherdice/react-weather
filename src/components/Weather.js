7ec416a6403b41f6627dd6be0294e2a9d64df800
Heather Dice <heatherdice22@gmail.com>
2024-04-26 08:49:08 -0400

    More responsive search bar

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -44,15 +44,9 @@ export default function Weather(props) {
     if (weatherData.ready) {
         return (
             <div className="Weather">
-                <form onSubmit={handleSubmit}>
-                    <div className="row">
-                        <div className="col-9">
-                            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" onChange={handleCityChange}/>
-                        </div>
-                        <div className="col-3">
-                            <input type="submit" value="search" className="btn w-100"/>
-                        </div>
-                    </div>
+                <form onSubmit={handleSubmit} className="input-group">
+                    <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" onChange={handleCityChange}/>
+                    <button type="button" className="btn" id="button-addon2">Search</button>
                 </form>
                 <WeatherInfo data={weatherData}/>
                 <Forecast coordinates={weatherData.coordinates}/>
