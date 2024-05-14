e4f64200d057bdda671175e3a30fbea5fba6f125
Heather Dice <heatherdice22@gmail.com>
2024-05-14 08:10:29 -0400

    Added alert message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Weather from "./components/Weather";
 import React from "react";
 
 export default function App() {
+  alert("Welcome! Please note that this page is currently under construction. As a result, you might experience some bugs. Rest assured, they're being squashed! Thanks for your patience!");
+
   return (
     <div className="App">
       <div className="container">
